b87a05c37eb4093b0edbf3f77aa568d395bf0b82
Aresyash01022000 <Yashkumar1.Jain@outlook.com>
2026-09-18 23:47:37 -0700

    Add AI financial chat interface

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Ship-a-ton AI backend is running!");
